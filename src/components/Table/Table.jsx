a1bfc99f3f9da2222a02cebcff751fe5b33abe6f
Chris Cantoni <chris.cantoni@gmail.com>
2024-03-02 11:54:55 -0600

    Created shuffle function.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -16,6 +16,20 @@ function Table() {
     const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     const allCards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
+    const shuffle = (cards) => {
+        console.log('clicked')
+        let currentOrder = allCards.length, randomShuffle;
+
+        while (currentOrder > 0) {
+            randomShuffle = Math.floor(Math.random() * currentOrder);
+            currentOrder--;
+
+            [cards[currentOrder], cards[randomShuffle]] = [cards[randomShuffle], cards[currentOrder]]
+        }
+        console.log('Shuffled cards', cards)
+        return cards;
+    }
+
 
 
   return(
@@ -23,8 +37,12 @@ function Table() {
       <h2>This is the Table!</h2>
       {/* Redux State isn't needed in the garden, it is just a parent component */}
       {/* Thanks to redux, there is no need to pass along props! */}
-      <p>{suits}</p>
-      <p>{allCards}</p>
+      <p>{suits.join(', ')}</p>
+      <p>{allCards.join(', ')}</p>
+      <button onClick={() => shuffle(allCards)}>Click me to shuffle</button>
+        <p></p>
+        <p>{cards}</p>
+
       <PlantForm />
       <PlantList />
     </div>
