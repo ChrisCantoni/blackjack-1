41605a4b27960865dd3c05bdadc85a526a02cf97
Chris Cantoni <chris.cantoni@gmail.com>
2024-03-04 13:50:50 -0600

    Added suits to shuffle and separated deck and shuffle into different functions.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,6 +7,7 @@ function Table() {
 
 
     const [shuffledCards, setShuffledCards] = useState([])
+    const [deck, setDeck] = useState([])
 
         // TODO: Here will be the shuffle dispatch
     // Shuffle itself will happen on the back end, yes?
@@ -14,23 +15,40 @@ function Table() {
     // First build the shuffle here.
     // Deal out two cards to both dealer and player
     // If dealer has 21, game over.
+   const refreshPage = () => {
+    console.log('refreshing page')
+   }
+    
+    useEffect(() => {
+        refreshPage()
+    }, [shuffledCards])
 
-    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    const allCards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
-    const shuffle = (cards) => {
+    const createDeck = () => {
+        let tempDeck = [];
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        const cards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
         console.log('clicked')
-        let currentOrder = allCards.length, randomShuffle;
-
-        while (currentOrder > 0) {
-            randomShuffle = Math.floor(Math.random() * currentOrder);
-            currentOrder--;
-
-            [cards[currentOrder], cards[randomShuffle]] = [cards[randomShuffle], cards[currentOrder]]
+        for (let card in cards) {
+            for (let suit in suits) {
+                console.log('add to deck', cards[card], ' of ', suits[suit]);
+                tempDeck.push(suits[suit] + ' ' + cards[card])                
+            }
         }
-        console.log('Shuffled cards', cards)
-        setShuffledCards(cards);
+        setDeck(...deck, tempDeck);
     }
+        
+    const shuffleDeck = (shuffleDeck) => {   
+        console.log('deck', shuffleDeck) 
+        for (let i = shuffleDeck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [shuffleDeck[i], shuffleDeck[j]] = [shuffleDeck[j], shuffleDeck[i]]
+        }
+        console.log('Shuffled cards', shuffleDeck)
+        setShuffledCards(shuffleDeck);
+    }
+
+    const dealCards = ()
 
 
 
@@ -39,14 +57,14 @@ function Table() {
       <h2>This is the Table!</h2>
       {/* Redux State isn't needed in the garden, it is just a parent component */}
       {/* Thanks to redux, there is no need to pass along props! */}
-      <p>{suits.join(', ')}</p>
-      <p>{allCards.join(', ')}</p>
-      <button onClick={() => shuffle(allCards)}>Click me to shuffle</button>
-        <p></p>
-        <p>{shuffledCards}</p>
+      {/* <p>{suits.join(', ')}</p>
+      <p>{cards.join(', ')}</p> */}
+      <button onClick={() => createDeck()}>Click me to create deck</button>
+      <button onClick={() => shuffleDeck(deck)}>Click me to shuffle</button>
+        <p>{deck}</p>
+        <p>{shuffledCards.join(', ')}</p>
 
-      <PlantForm />
-      <PlantList />
+      
     </div>
   )
 }
