ab0b732508640fd7ce6d0c32f8e8669908ca5f28
Chris Cantoni <chris.cantoni@gmail.com>
2024-03-22 14:50:17 -0500

    Working on dealer logic after player stays.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -12,7 +12,11 @@ function Table() {
     const [showCards, setShowCards] = useState(false)
     const [dealerHand, setDealerHand] = useState([])
     const [playerHand, setPlayerHand] = useState([])
-    const [welcome, setWelcome] = useState('Welcome!')
+    const [playerStatus, setPlayerStatus] = useState(true)
+    const [revealDealer, setRevealDealer] = useState(false);
+    const [gameStatus, setGameStatus] = useState(false)
+
+    const [cardCount, setCardCount] = useState(0);
         // TODO: Here will be the shuffle dispatch
     // Shuffle itself will happen on the back end, yes?
 
@@ -21,19 +25,17 @@ function Table() {
     // First build the shuffle here.
     // Deal out two cards to both dealer and player
     // If dealer has 21, game over.
-   const refreshPage = () => {
-    console.log('refreshing page')
-    console.log('player one hand', dealerHand)
-    console.log('player two hand', playerHand);
-   }
-    
-    useEffect(() => {
-        refreshPage();
-    }, [])
+
 
 
     const createDeck = () => {
+        if (gameStatus) {
+            Swal.fire('You already have a game in progress')
+            return;
+        }
         let tempDeck = [];
+        let i = 0;
+        while (i < 2) {
         const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         const cards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
         console.log('clicked')
@@ -42,6 +44,9 @@ function Table() {
                 tempDeck.push({suit: suits[suit], value: cards[card]})                
             }
         }
+        i++;
+    }
+        setCardCount(0);
         console.log('Deck is', tempDeck)
         setDeck(tempDeck);
     }
@@ -55,20 +60,12 @@ function Table() {
         return dealtCard;
     };
     
-    
-    
-    // const shuffleDeck = (shuffled) => {   
-    //     console.log('deck', shuffled)
-    //     for (let i = shuffled.length - 1; i > 0; i--) {
-    //         let j = Math.floor(Math.random() * (i + 1));
-    //         [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-    //     }
-    //     console.log('Shuffled cards', shuffled)
-    //     setShuffledCards(shuffled);
-    // }
-
     const dealCards = async () => {
-        if (deck.length == 0) {
+        if (deck.length != 0 && gameStatus) {
+            Swal.fire('You already have a game in progress');
+            return;
+        }
+        else if (deck.length == 0) {
             Swal.fire({
                 title: "You haven't shuffled the deck!",
                 text: "You need to click the button that says shuffle the deck",
@@ -80,6 +77,7 @@ function Table() {
               }).then((result) => {
                 if (result.isConfirmed) {
                   createDeck();
+                  setGameStatus(true)
                   Swal.fire({
                     title: "Shuffled!",
                     text: "Now try dealing those cards again.",
@@ -88,6 +86,7 @@ function Table() {
                 }
               });
         } else {
+            setGameStatus(true);
         for (let i = 0; i < 2; i++) {
             await new Promise(resolve => {
                 setTimeout(() => {
@@ -99,6 +98,13 @@ function Table() {
         }}
     };
 
+    const newGame = () => {
+        setGameStatus(false);
+        setPlayerStatus(true);
+        setPlayerHand([])
+        setDealerHand([])
+    }
+
     const hitCard = () => {
         setPlayerHand(prevHand => [...prevHand, dealRandomCards()])
     }
@@ -106,12 +112,14 @@ function Table() {
     const playerStay = () => {
         // trigger dealer actions to hit or stay and determine winner
         // Separate dealer and player totals into their own states?
+        // Move to next player?
+        console.log('reveal dealer')
+        setRevealDealer(true);
     }
 
     const calculateValue = (hand) => {
         let total = 0;
         for (let card of hand) {
-
             if (typeof card.value != 'string') {
                 total += card.value
             }
@@ -125,40 +133,79 @@ function Table() {
                         total -= 11;
                         total += 1;
                     }
-                }
-            }
-        }
+                }}}
         return total;
     }
 
-    // TODO: Add logic for dealer to hit or stay
+    const calculateDealer = (hand) => {
+        let total = 0;
+        if (typeof hand[0].value == 'string') {
+            total += 10;
+        } else {
+            total = hand[0].value;
+        }
+    }
 
+    const updatePlayerStatus = () => {
+        if (calculateValue(playerHand) > 21) {
+            setPlayerStatus(!playerStatus)
+        }
+    }
+
+    const updateDealerStatus = () => {
+
+    }
+
+    const countCard = () => {
+        let count = cardCount;
+        for (let card of playerHand) {
+            if (typeof card.value != 'string' && card.value < 10) {
+                count +=1;
+            } else {
+                count -=1;
+            }
+        }
+        setCardCount(count);
+    }
+
+
+    useEffect(() => {
+        updatePlayerStatus()
+        countCard()
+        calculateDealer(dealerHand)
+    },[playerHand])
 
   return(
     <div>
       <h2>This is the Table!</h2>
       {/* <p>{suits.join(', ')}</p>
       <p>{cards.join(', ')}</p> */}
-      <h3>{welcome}</h3>
       <button onClick={() => createDeck()}>Shuffle the deck</button>
       <button onClick={() => dealCards()}>Deal Cards</button>
       
       <div>
         <p>Dealer hand: {JSON.stringify(dealerHand)}.</p>
-        <p>Dealer hand: {dealerHand.length > 0 ? `${dealerHand[0].value} of ${dealerHand[0].suit}` : ''}
-        {dealerHand.length > 0 && dealerHand[0].value == 'A' && calculateValue(dealerHand) == 21 ? <h2>Blackjack! Dealer wins!</h2> : ''}
-        </p>
-        <p>Player hand: {JSON.stringify(playerHand)}</p>
+        <p>Dealer hand: {dealerHand.length > 0 && revealDealer ? dealerHand.map((card, index) => (
+                        <>{card.value} of {card.suit}{index < dealerHand.length - 1 ? ', ' : ''} </>
+                    ))  : dealerHand.length > 0 ? `${dealerHand[0].value} of ${dealerHand[0].suit}` : ''}
+                    <h3>Total: {dealerHand.length > 0 && revealDealer ? calculateValue(dealerHand) : dealerHand.length > 0 ? calculateDealer(dealerHand) : ''}</h3>
+                    </p>
+
         <p>Player hand: {playerHand.length > 0 ? playerHand.map((card) => {
             return (
-                <p>{card.value} of {card.suit}</p>)}) : ''}
+                <>{card.value} of {card.suit} </>)}) : ''}
                 Total: {calculateValue(playerHand)}
-                {calculateValue(playerHand) > 21 ? <h2>You lose!</h2> : 
-                <>
+                {playerStatus ? <>
                     <button onClick={() => hitCard()}>Hit</button>
                     <button onClick={() => playerStay()}>Stay</button>
-                </>}
+                </> :
+                <>
+                    <h2>You Lose!</h2>
+                    <button onClick={() => newGame()}>New Game</button>
+                </>
+                }
         </p>
+        <h3>Current Card Count: {cardCount}</h3>
         </div>
       
     </div>
